8ba3da0d38a78ffbd0395e004bcfbae38035ca39
Anabellit <150425047+Anabellit@users.noreply.github.com>
2024-06-27 15:31:40 +0200

    US2 Implementierung: See current state of the game, Notify game end (win, loss, draw), Start a new game after current game ends

--- tictactoegame.java
+++ tictactoegame.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 
 public class tictactoegame {
@@ -29,7 +28,7 @@ public class tictactoegame {
         System.out.println("Spieler X beginnt.");
 
         while (gameRunning) {
-            printBoard();
+            printGameState();
             System.out.println("Spieler " + currentPlayer + ", gib deine Zugkoordinaten ein (Zeile Spalte):");
 
             int row = scanner.nextInt();
@@ -37,8 +36,18 @@ public class tictactoegame {
 
             if (isValidMove(row, col)) {
                 board[row][col] = currentPlayer;
-                // Weitere Logik wird später hinzugefügt
-                currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+
+                if (checkForWin()) {
+                    printGameState();
+                    System.out.println("Herzlichen Glückwunsch! Spieler " + currentPlayer + " hat gewonnen!");
+                    gameRunning = false;
+                } else if (isBoardFull()) {
+                    printGameState();
+                    System.out.println("Unentschieden! Das Spiel endet.");
+                    gameRunning = false;
+                } else {
+                    currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+                }
             } else {
                 System.out.println("Ungültiger Zug! Bitte gib gültige Koordinaten ein.");
             }
@@ -47,7 +56,8 @@ public class tictactoegame {
         scanner.close();
     }
 
-    private void printBoard() {
+    public void printGameState() {
+        System.out.println("Aktueller Spielstand:");
         System.out.println("-------------");
         for (int i = 0; i < 3; i++) {
             System.out.print("| ");
@@ -57,11 +67,43 @@ public class tictactoegame {
             System.out.println();
             System.out.println("-------------");
         }
-
     }
 
     private boolean isValidMove(int row, int col) {
         return row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == '-';
+    }
+
+    private boolean checkForWin() {
+        // Überprüfe horizontale und vertikale Linien
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] != '-' && board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
+                return true;
+            }
+            if (board[0][i] != '-' && board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
+                return true;
+            }
+        }
+
+        // Überprüfe diagonale Linien
+        if (board[0][0] != '-' && board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+            return true;
+        }
+        if (board[0][2] != '-' && board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean isBoardFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == '-') {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
